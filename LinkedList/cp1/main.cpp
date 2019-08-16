0ed3c4fc0ebcbcbd3a8f2dc26d7d352448baa2fc
yshr1982 <>
2019-08-16 21:59:17 +0900

    sort and insert

--- LinkedList/cp1/main.cpp
+++ LinkedList/cp1/main.cpp
@@ -7,9 +7,6 @@
 
 int main(void){
     class my_NodeList temp;
-    class my_NodeList result;
-    class Node *pNode;
-    int *pList;
 
     // Create a list filled with random numbers.
     for(unsigned int iter = 0 ; iter < LIST_NUM ; iter++){
@@ -18,17 +15,8 @@ int main(void){
     temp.show();
     std::cout << "-----------------" << std::endl;
     
-    pNode = temp.head;
-    pList = (int *)malloc((sizeof(int)*MAX_NUMBER));
-    memset(pList,0,sizeof(int)*MAX_NUMBER);
-    while(pNode != NULL){
-        if(pList[pNode->data] == 0){
-            pList[pNode->data] = 1;
-            result.apendToTail(pNode->data);
-        }
-        pNode = pNode->next;
-    }
-    result.show();
+    temp.sortNode(20);
+    temp.show();
 
 
     return 0;
